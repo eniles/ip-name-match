176d9d28824c967ac3f540b0c292300447ab3f64
Ed Niles <edniles@gmail.com>
2018-02-08 17:30:33 -0600

    get redshift connection working

--- run_lookup.py
+++ run_lookup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import sys
+from datetime import *
 import urllib
 import json
 import time
 import re
+#import pandas
+import psycopg2
+import mycreds
+
 
 def get_info(adress):
     #print("************************************************")
@@ -15,8 +20,8 @@ def get_info(adress):
         #result = result[2:len(result)-3]
         result = json.loads(result)
     except Exception as e:
-        print("Could not find: ", adress)
-        print e
+        print("Could not find: ", address)
+        print(e)
         print(result)
         return None
 
@@ -33,6 +38,27 @@ def get_info(adress):
     print("Location link: " + "http://www.openstreetmap.org/#map=11/" + str(result["latitude"]) +"/" + str(result["longitude"]))
     return result
 
+def redshift_query_getter(query):
+    #Return data from str query from Redshift
+    start = datetime.now()
+    #Obtaining the connection to RedShift
+    connection_string = "dbname='" + mycreds.redshift_db['dbname'] + "' port='5439' user='" + mycreds.redshift_db['user'] + "' password='" + mycreds.redshift_db['pass'] + "' host='" + mycreds.redshift_db['dburl'] + "'";
+    print "Connecting to \n        ->%s" % (connection_string)
+    conn = psycopg2.connect(connection_string);
+    cur = conn.cursor()
+
+    ## DO THE THINGS
+    cur.execute(query)
+    for row in cur.fetchall():
+        print(row)    
+
+    cur.close();
+    conn.commit();
+    conn.close();
+    t_0 = datetime.now() - start
+    print('Time to fetch data: %s' % t_0)
+    return None
+
 def showhelp():
     print ("Usage: geoip address [address]...")
     print ("find gelocation of IP addresses and host names, using http://freegeoip.net/")
@@ -44,6 +70,10 @@ if __name__ == "__main__": #code to execute if called from command-line
     else:
         #print(inputs[1])
         print("Let's get started...")
+
+        sql_query = "SELECT state_code FROM " + mycreds.tablename1 + " WHERE deceased!=1 LIMIT 10;"
+        redshift_query_getter(sql_query)
+
         for line in open(inputs[1]):
         #for address in inputs[1:]:
             csv_row = line.split(",") #returns a list ["1","50","60"]
