45a707076b8466acb608de9543a9019bc36d8248
Ed Niles <edniles@gmail.com>
2018-02-08 00:47:33 -0600

    Read in CSV of names and IP addresses
    Add test csv file
    update readme
    add CSV file reader and do an IP address lookup on each line

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ip-name-match
+
+Python script to read in data from a CSV file and look up City/Zip for an IP from freegeoip.net
+Expects a CSV file with ID in first column, FULLNAME in third column, and IPADDR in sixth column
--- run_lookup.py
+++ run_lookup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import sys
-import urllib.request
+import urllib
 import json
+import time
+import re
 
 def get_info(adress):
-    print("************************************************")
+    #print("************************************************")
 
     api = "http://freegeoip.net/json/" + adress
     try:
-        result = urllib.request.urlopen(api).read()
+        result = urllib.urlopen(api).read()
         result = str(result) 
-        result = result[2:len(result)-3]
+        #result = result[2:len(result)-3]
         result = json.loads(result)
-    except:
+    except Exception as e:
         print("Could not find: ", adress)
+        print e
+        print(result)
         return None
 
     print(adress)
@@ -27,6 +31,7 @@ def get_info(adress):
     print("Latitude: ", result["latitude"])
     print("Longitude: ", result["longitude"])
     print("Location link: " + "http://www.openstreetmap.org/#map=11/" + str(result["latitude"]) +"/" + str(result["longitude"]))
+    return result
 
 def showhelp():
     print ("Usage: geoip address [address]...")
@@ -37,6 +42,19 @@ if __name__ == "__main__": #code to execute if called from command-line
     if len(inputs) < 2 or "--help" in inputs:
         showhelp()
     else:
-        for address in inputs[1:]:
-            get_info(address)
+        #print(inputs[1])
+        print("Let's get started...")
+        for line in open(inputs[1]):
+        #for address in inputs[1:]:
+            csv_row = line.split(",") #returns a list ["1","50","60"]
+            #print(csv_row)
+            if (csv_row[2] and csv_row[6]
+                and re.match(r'^((\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])$', csv_row[6])
+                ):
+                print(csv_row[2] + " " + csv_row[6])
+                time.sleep(15)
+                ip_lookup=get_info(csv_row[6])
+                #print(ip_lookup)
+                print("Returned City: ", ip_lookup["city"])
+
         print("************************************************")
